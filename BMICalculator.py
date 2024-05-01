37870c8f85c8fee47ed1f02033a1159a5d671795
DenizAcar <acard122433@gmail.com>
2024-05-01 13:12:32 +0300

    bmi calculator version 1.2

--- BMICalculator.py
+++ BMICalculator.py
@@ -1,48 +1,67 @@
-from tkinter import *
+import tkinter
 
-window = Tk()
+window = tkinter.Tk()
 window.title("BMI Calculator")
-window.minsize(width=200, height=150)
+window.config(padx=30, pady=30)
 
-weight_label = Label(text="Enter your weight(kg)", font=("Helvetica", 10, "normal"))
+weight_label = tkinter.Label(text="Enter your weight(kg)", font=("Helvetica", 10, "normal"))
 weight_label.pack()
-weight_entry = Entry(width=10)
+
+weight_entry = tkinter.Entry(width=10)
 weight_entry.pack()
 
-height_label = Label(text="Enter your height(m)", font=("Helvetica", 10, "normal"))
+height_label = tkinter.Label(text="Enter your height(cm)", font=("Helvetica", 10, "normal"))
 height_label.pack()
-height_entry = Entry(width=10)
+
+height_entry = tkinter.Entry(width=10)
 height_entry.pack()
 
-result_label = Label()
-result_label.config(font=("Helvetica", 10, "normal"))
 
 
-def button_clicked():
-    if height_entry == int or height_entry == float and weight_entry == int or height_entry == float:
-        bmi_value = float(int(weight_entry.get()) / float(height_entry.get()) ** 2)
-        if bmi_value <= 18.5:
-            result_label.config(text=f"Your BMI is {bmi_value}.You are underweight")
 
-        elif bmi_value > 18.5 and bmi_value <= 24.9:
-            result_label.config(text=f"Your BMI is {bmi_value}.Your weight is healthy")
-
-        elif bmi_value > 24.9 and bmi_value <= 29.9:
-            result_label.config(text=f"Your BMI is {bmi_value}.You are overweight")
-
-        elif bmi_value > 29.9:
-            result_label.config(text=f"Your BMI is {bmi_value}.You are obese")
-
-        else:
-            print("Please enter valid values")
+def calculate_bmi():
+    weight = weight_entry.get()
+    height = height_entry.get()
+    if height == "" or weight == "":
+        result_label.config(text="Enter both height and weight!")
 
     else:
-        result_label.config(text="Please enter valid numbers")
+        try:
+            bmi = float(weight) / (float(height) / 100) ** 2
+            result_string = write_result(bmi)
+            result_label.config(text=result_string)
+        except:
+            result_label.config(text="Enter a valid number!")
+def write_result(bmi):
+    result_string = f"Your BMI is {round(bmi, 2)}. You are "
+    if bmi <= 16:
+        result_string += "severely thin!"
+    elif 16 < bmi <= 17:
+        result_string += "moderately thin!"
+    elif 17 < bmi <= 18.5:
+        result_string += "mild thin!"
+    elif 18.5 < bmi <= 25:
+        result_string += "normal weight!"
+    elif 25 < bmi <= 30:
+        result_string += "overweight!"
+    elif 30 < bmi <= 35:
+        result_string += "obese class 1"
+    elif 35 < bmi <= 40:
+        result_string += "obese class 2"
+    else:
+        result_string += "obese class 3"
+    return result_string
 
 
-my_button = Button(text="Calculate", command=button_clicked)
-my_button.pack()
-
+calculate_button = tkinter.Button(text="Calculate", command=calculate_bmi)
+calculate_button.pack()
+result_label = tkinter.Label(font=("Helvetica", 8, "normal"))
 result_label.pack()
 
+
+
+
 window.mainloop()
+
+
+
